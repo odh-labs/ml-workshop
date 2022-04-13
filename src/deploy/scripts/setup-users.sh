3f5c36d2d9bc82a88c1dcefeb3f33d78ae025ae7
Faisal Masood <fmasood@redhat.com>
2022-04-13 14:54:09 +1000

    add a new htpasswd provider to save the opentlcmgr user from being screwed 😱

--- src/deploy/scripts/setup-users.sh
+++ src/deploy/scripts/setup-users.sh
@@ -1,13 +1,26 @@
 #! /bin/bash
 
 cd $REPO_HOME/scripts
+oc delete secret htpasswd-secret -n openshift-config
+oc create secret generic htpasswd-secret --from-file=htpasswd=pre-bakedusers.httpasswd -n openshift-config
+
 oc create secret generic htpass-secret --from-file=htpasswd=users.htpasswd -n openshift-config
 
-oc apply -f htpasswd.cr
+oc apply -f htpasswd-oauth.yaml
+
+# the application of the oauth object will trigger the bounce of the auth pods in openshift-auth ns
+#oc scale deployment/oauth-deployment --replicas=0 -n openshift-authentication
+#oc scale deployment/oauth-deployment --replicas=3 -n openshift-authentication
+oc rollout status deployment/oauth-openshift --watch=true -n openshift-authentication
+
+
+
+oc create ns ml-workshop
 
 for i in {1..30}
 do
     oc adm policy add-role-to-user admin user$i -n ml-workshop
 done
 oc adm policy add-cluster-role-to-user cluster-admin user29
+
 
